4dcaeb31f4594672ab2f0a01657a1685ae5cfc93
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2022-06-05 16:40:30 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let number = n.toString();
+  if (number[0] !== '-') {
+    return number.split('').reverse().join('');
+  } else return number.slice(1).split('').reverse().join('');
 }
